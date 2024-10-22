f0141582d3f7cd6d5bd9d644bb2e70ea77c1092c
Lance Miller <lance@miller.pub>
2024-10-22 12:25:39 -0600

    fix formatting

--- apps/miller.pub/eslint.config.js
+++ apps/miller.pub/eslint.config.js
@@ -5,16 +5,16 @@ const nx = require('@nx/eslint-plugin');
 const baseConfig = require('../../eslint.config.js');
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
+	baseDirectory: __dirname,
+	recommendedConfig: js.configs.recommended,
 });
 
 module.exports = [
-  ...fixupConfigRules(compat.extends('next')),
+	...fixupConfigRules(compat.extends('next')),
 
-  ...fixupConfigRules(compat.extends('next/core-web-vitals')),
+	...fixupConfigRules(compat.extends('next/core-web-vitals')),
 
-  ...baseConfig,
-  ...nx.configs['flat/react-typescript'],
-  { ignores: ['.next/**/*'] },
+	...baseConfig,
+	...nx.configs['flat/react-typescript'],
+	{ ignores: ['.next/**/*'] },
 ];
