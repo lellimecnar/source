da191a3ad93ddd84d160b4b009a5a59f19254ced
Lance Miller <lance@miller.pub>
2024-10-28 12:02:47 -0600

    adjust theme

--- apps/miller.pub/src/app/layout.tsx
+++ apps/miller.pub/src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({ children }: RootLayoutProps): JSX.Element {
 			<head />
 			<body
 				className={cn(
-					'min-h-screen bg-background font-sans antialiased',
+					'min-h-screen bg-sky-950 font-sans antialiased',
 					fontSans.variable,
 				)}
 			>
--- apps/miller.pub/src/components/site-header/site-header.tsx
+++ apps/miller.pub/src/components/site-header/site-header.tsx
@@ -19,7 +19,7 @@ import { siteConfig, type LinkItem } from '@/config/site';
 
 export function SiteHeader(): JSX.Element {
 	return (
-		<header className="sticky top-0 z-40 w-full bg-background">
+		<header className="sticky top-0 z-40 w-full bg-sky-900 border-b-2 border-sky-800">
 			<div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
 				<div className="flex gap-6 md:gap-10">
 					<Link href="/" className="flex items-center space-x-2">
