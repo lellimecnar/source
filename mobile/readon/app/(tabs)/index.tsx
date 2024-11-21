f5dc30590df0a45c22b0f971bca0333d0a77b009
Lance Miller <lance@miller.pub>
2024-11-20 20:21:01 -0700

    use FlatList for cards

--- mobile/readon/app/(tabs)/index.tsx
+++ mobile/readon/app/(tabs)/index.tsx
@@ -3,16 +3,16 @@ import { FlatList, Text, View } from 'react-native';
 const cards = Array.from({ length: 12 }).map((_, i) => i + 1);
 export default function TabOneScreen(): JSX.Element {
 	return (
-		<View className="container justify-start">
+		<View className="container justify-start py-0">
 			<FlatList
 				data={cards}
-				className="flex flex-col gap-2"
+				className="flex flex-col py-2"
 				renderItem={({ item }) => (
-					<View
-						className={`card min-h-32 w-full ${item > 1 ? 'mt-2' : ''}`}
-						key={`card-${item}`}
-					>
+					<View className="card mb-2 min-h-32 w-full" key={`card-${item}`}>
 						<Text className="text-xl font-bold">Card {item}</Text>
+						<Text className="text-sm">Some stuff</Text>
+						<Text className="text-sm">Some more stuff</Text>
+						<Text className="text-sm">Some other stuff</Text>
 					</View>
 				)}
 			/>
