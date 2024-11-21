0fa8cd37bfdd9935bd69f64f3339cd6c0a3733f9
Lance Miller <lance@miller.pub>
2024-11-20 20:14:13 -0700

    add card list

--- mobile/readon/app/(tabs)/index.tsx
+++ mobile/readon/app/(tabs)/index.tsx
@@ -1,9 +1,21 @@
-import { Text, View } from 'react-native';
+import { FlatList, Text, View } from 'react-native';
 
-export default function TabOneScreen() {
+const cards = Array.from({ length: 12 }).map((_, i) => i + 1);
+export default function TabOneScreen(): JSX.Element {
 	return (
-		<View className="container">
-			<Text className="text-xl font-bold !text-red-700">Tab One</Text>
+		<View className="container justify-start">
+			<FlatList
+				data={cards}
+				className="flex flex-col gap-2"
+				renderItem={({ item }) => (
+					<View
+						className={`card min-h-32 w-full ${item > 1 ? 'mt-2' : ''}`}
+						key={`card-${item}`}
+					>
+						<Text className="text-xl font-bold">Card {item}</Text>
+					</View>
+				)}
+			/>
 		</View>
 	);
 }
