814b7e95bb86a54f920e9e53834c14778eff0718
Lance Miller <lance@miller.pub>
2025-12-21 21:49:24 -0700

    implement type checking scripts

--- card-stack/core/src/card-set/types.ts
+++ card-stack/core/src/card-set/types.ts
@@ -1,5 +1,7 @@
+import { type List } from '@lellimecnar/utils';
+
 import { type Card } from '..';
 
 export type CardSetFilterPredicate = Parameters<
-	typeof import('@lellimecnar/utils').remove<Card>
+	typeof import('@lellimecnar/utils').remove<List<Card>>
 >[1];
--- web/miller.pub/jest.config.js
+++ web/miller.pub/jest.config.js
@@ -1,4 +1,4 @@
-const nextJest = require('next/jest');
+const nextJest = require('next/jest').default;
 
 const createJestConfig = nextJest({
 	// Provide the path to your Next.js app to load next.config.js and .env files in your test environment
